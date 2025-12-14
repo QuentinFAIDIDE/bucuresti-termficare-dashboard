31b35e86bb216aedc07f89bafd5ed305bf05e700
Quentin Faidide <quentin.faidide@gmail.com>
2025-12-14 02:39:21 +0200

    fix: roll back broken tile provider

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ export const MAP_CONFIG = {
   center: [44.4268, 26.1025] as [number, number],
   zoom: 13,
   tileUrl:
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
+    "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
   attribution:
-    '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 } as const;
